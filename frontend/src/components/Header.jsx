778a8f3be6fdc5014dc11b1acfb531a4f5d595c5
Akash <akash98161615@gmail.com>
2022-09-11 08:25:09 +0530

    sidebar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,35 @@
 import React from "react";
-import LogoBg from '../assets/images/LogoBgImg.svg';
+import LogoBg from "../assets/images/LogoBgImg.svg";
 
-const Header = ({setLogoutView,logoutView}) => {
+const Header = ({ setLogoutView, logoutView }) => {
   return (
     <header>
-      <div className="container mx-auto">
+      <div>
         <div className="flex justify-between h-24 items-center">
           <div className="relative">
-          <h1 className="text-4xl bg-primary text-white absolute top-20 left-10 z-50">Connect</h1>
+            <h1 className="text-4xl bg-primary text-white absolute top-20 left-10 z-50">
+              Connect
+            </h1>
             <img src={LogoBg} alt="" className="w-60" />
           </div>
           <div className="flex">
-            <p onClick={()=>setLogoutView("login")} className={`text-white text-xl cursor-pointer mr-10 ${logoutView!=="sign" &&"text-primary underline"}`}>Login</p>
-            <p onClick={()=>setLogoutView("sign")} className={`text-white text-xl cursor-pointer mr-10 ${logoutView==="sign" &&"text-primary underline"}`}>SignUp</p>
-            </div>
+            <p
+              onClick={() => setLogoutView("login")}
+              className={`text-white text-xl cursor-pointer font-bold mr-10 ${
+                logoutView !== "sign" && "text-primary underline"
+              }`}
+            >
+              Login
+            </p>
+            <p
+              onClick={() => setLogoutView("sign")}
+              className={`text-white text-xl cursor-pointer font-bold mr-10 ${
+                logoutView === "sign" && "text-primary underline"
+              }`}
+            >
+              SignUp
+            </p>
+          </div>
         </div>
       </div>
     </header>
